ebf788e7e2c34cf82151cf7d1c73ef13404323d8
Dan <danisrael@dandandino.com>
2019-05-01 15:14:14 +0800

    modal styling and JS functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,20 +2,64 @@ var modal = {
 
     declaration: function(){
 
-        // modal.open('.s-modal');
-        var modalButtons = document.getElementsByClassName(".btn-modal");
-        modalButtons.onclick = function(e){
-            // e.preventDefault();
-            alert('sdfsd');
-        };
+        modal.ready();
 
     },
 
-    open: function(className){
 
-        // $('.' + className).addClass('active');
-        // console.log('hello');
-    }
+    ready: function(){
+
+        document.body.onclick = function(e) {  
+
+            e = window.event? event.srcElement : e.target;
+        
+            if( e.className && e.className.indexOf('btn-modal') != -1 ) {
+                
+                modal.open(e);
+            }
+            else if( e.className && e.className.indexOf('modal-close') != -1 ){
+
+                modal.closeActive();
+            }
+        }
+
+
+        document.addEventListener('keydown', function(e) {
+            if( e.keyCode == 27 ){
+                modal.closeActive();
+            }
+        })
+
+    },
+
+    open: function(e){
+
+        if( e.getAttribute('data-target') ){
+                    
+            var target = document.getElementById( e.getAttribute('data-target') );
+            var node = document.createElement("div"); 
+
+            target.className += ' active';
+
+            node.id = 's-modal-bg';
+
+            document.body.appendChild(node);
+        }
+    },
+
+    closeActive: function(){
+
+        var x = document.getElementsByClassName("s-modal active");
+        if( x ){
+            
+            for (i = 0; i < x.length; i++) {
+                x[i].className = x[i].className.replace('active', '');
+            }
+        }
+
+        document.getElementById('s-modal-bg').remove();
+    },
+
 };
 
 
